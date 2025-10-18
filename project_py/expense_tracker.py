41ee41d024a39a6c7ba327aaf292ef8ec5ad135f
subasri-sha <subashankar2004@gmail.com>
2025-10-18 15:01:24 +0530

    refactor summary with daily budget calculation

--- project_py/expense_tracker.py
+++ project_py/expense_tracker.py
@@ -1,32 +1,34 @@
 from expense import Expense
 import os
 from typing import List
+import datetime
+import calendar
 
 def main():
     print("🚀Running Expense Tracker💸")
     budget =  20000
 
     # Get user input for expense.
-    # expense = get_user_expense()
+    expense = get_user_expense()
     expense_file_path = "expenses.csv"
 
     # Write their expense csv file.
-    # write_expense_file(expense, expense_file_path)
+    write_expense_file(expense, expense_file_path)
 
     # Read and Summarize their expense.
     summarize_expense(expense_file_path, budget)
 
 
 def get_user_expense():
-    print("Getting user expense ")
-    expense_name = input("Enter your expense name: ")
-    expense_amount = float(input("Enter your expense amount: "))
+    print("Getting User Expense ")
+    expense_name = input("Enter Your Expense Name: ")
+    expense_amount = float(input("Enter Your Expense Amount: "))
    
    # creating expense category in a list
 
     expense_categories = [
         "🏠 Rent",
-        "🍽️ Food",
+        "🍽️  Food",
         "💊 Medical Bills",
         "💰 Savings",
         "🧾 Miscellaneous"
@@ -40,7 +42,7 @@ def get_user_expense():
 
         value_range = f"[1 - {len(expense_categories)}]" # To enter user from the range of 1 to 5
 
-        selected_index = int(input(f"Enter a category number {value_range}: ")) - 1 # -1 is to 0-based indexing in Python
+        selected_index = int(input(f"Enter a Category Number {value_range}: ")) - 1 # -1 is to 0-based indexing in Python
       
 
         if selected_index in range(len(expense_categories)):
@@ -48,7 +50,7 @@ def get_user_expense():
             new_expense = Expense(name=expense_name, category=selected_category, amount=expense_amount)
             return new_expense
         else:
-            print("Invalid Category. Enter a valid number")
+            print("Invalid Category. Enter a Valid Number")
         
 
 
@@ -60,13 +62,13 @@ def write_expense_file(expense, expense_file_name = "expenses.csv"):
     # Create the full path inside project_py folder
     expense_file_path = os.path.join(current_dir, expense_file_name)
 
-    print(f"Saving your expense file: {expense} to {expense_file_path} ")
+    print(f"Saving Your Expense File: {expense} to {expense_file_path} ")
     with open(expense_file_path, 'a', encoding='utf-8') as f:
         f.write(f"{expense.name}, {expense.category}, {expense.amount}\n")
     
 
 def summarize_expense(expense_file_path, budget):
-    print(f"💰Summarizing your expense ")
+    print(f"🔍Summarizing Your Expense ")
     expenses: List[Expense] = [ ]
     current_dir = os.path.dirname(os.path.abspath(__file__)) # To save csv file in the same directory as same as other py. files
     expense_file_path = os.path.join(current_dir, "expenses.csv")
@@ -107,9 +109,22 @@ def summarize_expense(expense_file_path, budget):
     remaining_budget = budget - total_spent
     print(f"🎯 Remaining budget: {remaining_budget: .2f}")
 
+    # Get the current date
+    now = datetime.datetime.now()
 
+    #Get the no.of days in the current month
+    days_in_month = calendar.monthrange(now.year, now.month)[1]
 
-    
+    # Calculate the remaining number of days in the current month
+
+    remaining_days = days_in_month - now.day
+    print("📅 Remaining Days in the current month: ", remaining_days)
+
+    daily_budget = remaining_budget / remaining_days
+    print(green(f"👉 Budget Per Day:  ₹{daily_budget: .2f}"))
+
+def green(text):
+    return f"\033[92m{text}\033[0m"
 
 
 
