37bfb790d720516a68be3f40411c8e63d2397c79
subasri-sha <subashankar2004@gmail.com>
2025-10-18 14:41:12 +0530

    refactor to write and read expense csv file

--- project_py/expense.py
+++ project_py/expense.py
@@ -2,10 +2,12 @@ class Expense:
     def __init__(self, name, category, amount):
         self.name = name
         self.category = category
-        self.amount = amount
+        # self.amount = amount
+        self.amount = float(str(amount).replace("Rs.", "").replace("₹", "").strip())
+
 
     def __repr__(self):
-        return f"<Expense: {self.name}, {self.category}, ${self.amount:.2f} >"
+        return f"<Expense: {self.name}, {self.category}, {self.amount:.2f} >"
     
     '''
     <expense.Expense object at 0x000001D8F62D7230> to avoid this using def __repr__(self): --> Returns a string representation of the object when you print it. 
--- project_py/expense_tracker.py
+++ project_py/expense_tracker.py
@@ -1,17 +1,20 @@
 from expense import Expense
+import os
+from typing import List
 
 def main():
-    print("Running Expense Tracker! ")
+    print("🚀Running Expense Tracker💸")
+    budget =  20000
 
     # Get user input for expense.
-    expense = get_user_expense()
-    print(expense)
+    # expense = get_user_expense()
+    expense_file_path = "expenses.csv"
 
     # Write their expense csv file.
-    write_expense_file()
+    # write_expense_file(expense, expense_file_path)
 
     # Read and Summarize their expense.
-    summarize_expense()
+    summarize_expense(expense_file_path, budget)
 
 
 def get_user_expense():
@@ -23,7 +26,7 @@ def get_user_expense():
 
     expense_categories = [
         "🏠 Rent",
-        "🍽️  Food",
+        "🍽️ Food",
         "💊 Medical Bills",
         "💰 Savings",
         "🧾 Miscellaneous"
@@ -50,12 +53,65 @@ def get_user_expense():
 
 
 
-def write_expense_file():
-    print("Writing expense file")
+def write_expense_file(expense, expense_file_name = "expenses.csv"):
+    # Get the directory where this script is located
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Create the full path inside project_py folder
+    expense_file_path = os.path.join(current_dir, expense_file_name)
+
+    print(f"Saving your expense file: {expense} to {expense_file_path} ")
+    with open(expense_file_path, 'a', encoding='utf-8') as f:
+        f.write(f"{expense.name}, {expense.category}, {expense.amount}\n")
     
 
-def summarize_expense():
-    print("Read and Summarize their expense")
+def summarize_expense(expense_file_path, budget):
+    print(f"💰Summarizing your expense ")
+    expenses: List[Expense] = [ ]
+    current_dir = os.path.dirname(os.path.abspath(__file__)) # To save csv file in the same directory as same as other py. files
+    expense_file_path = os.path.join(current_dir, "expenses.csv")
+    with open(expense_file_path, "r", encoding="utf-8") as f: # "utf-8 --> Explicitly tell Python to read the file as UTF-8 (the same encoding you used when writing it)."
+        lines = f.readlines()
+        for line in lines:
+            if not line.strip():
+                continue  # skip empty lines
+            parts = [x.strip() for x in line.strip().split(",")]
+            if len(parts) != 3:
+                print("⚠️ Skipping invalid line:", line.strip())
+                continue
+
+            expense_name, expense_category, expense_amount = parts
+
+            line_expense = Expense(
+                name=expense_name, category=expense_category, amount=expense_amount
+            )
+            
+            expenses.append(line_expense)
+    
+    amount_by_category = { }
+
+    for expense in expenses:
+        key = expense.category # Use the category of the expense as the key for grouping/summing
+        if key in amount_by_category:  # Check if this category already exists in the dictionary
+            amount_by_category[key] += expense.amount # if the key exists it update to the existing value
+        else: 
+            amount_by_category[key] = expense.amount #If Not, initialize the category in the dictionary with this expense amount
+    print("📊 Expenses by Category: ")
+
+    for key, amount in amount_by_category.items():
+        print(f"  {key: <20}:  ₹{amount: .2f}")
+
+    total_spent = sum([ex.amount for ex in expenses])
+    print(f"💸 Total Spent: ₹{total_spent: .2f}")
+
+    remaining_budget = budget - total_spent
+    print(f"🎯 Remaining budget: {remaining_budget: .2f}")
+
+
+
+    
+
+
 
 
 if __name__ == "__main__": # This condition checks whether the script is being run directly (not imported as a module into another script).
