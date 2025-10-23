d81a879c029dab3e87d68e802ba5086b773ea156
subasri-sha <subashankar2004@gmail.com>
2025-10-23 20:13:18 +0530

    new portfolio

--- model/main.py
+++ model/main.py
@@ -1,15 +1,29 @@
+# import pandas as pd
+
+# def get_clean_data(): # Reads data from data.csv
+#     data = pd.read_csv("data/data.csv")
+#     print(data.head())
+#     return data
+
+# def main():
+#   data = get_clean_data()
+ 
+ 
+# #
+
+# if __name__ == '__main__':
+#     main()
+
+
 import pandas as pd
 
-def get_clean_data(): # Reads data from data.csv
-    data = pd.read_csv("C:\\pynotes.a\\data.csv.csv")
+def get_clean_data():  # Reads data from data.csv
+    data = pd.read_csv("C:\\pynotes.a\\data.csv")  # make sure file name is correct
     print(data.head())
-    # return data
+    return data
 
 def main():
+    get_clean_data()
 
-
-
- if __name__ == '__main__':
+if __name__ == '__main__':
     main()
-
-
