6cc7e0c22fb42edb5e9c3a890f4ad52812f5a62c
subasri-sha <subashankar2004@gmail.com>
2025-10-23 20:11:50 +0530

    first commit

---
+++ model/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def get_clean_data(): # Reads data from data.csv
+    data = pd.read_csv("C:\\pynotes.a\\data.csv.csv")
+    print(data.head())
+    # return data
+
+def main():
+
+
+
+ if __name__ == '__main__':
+    main()
+
+
